d6b217eaa1368d099df55891e98709086e1c13aa
Eric Youngmin Song <dudals2900@gmail.com>
2021-10-25 20:59:53 -0400

    Firebase settings

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRSZoDulLtUAiee-elRSGXPKG_qZvyuLQ",
+  authDomain: "nwitter-89e76.firebaseapp.com",
+  projectId: "nwitter-89e76",
+  storageBucket: "nwitter-89e76.appspot.com",
+  messagingSenderId: "181813688555",
+  appId: "1:181813688555:web:0323cc798591ada49c26ae",
+};
+
+export default firebase.initializeApp(firebaseConfig);
